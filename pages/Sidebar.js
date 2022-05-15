3c66d26d8201def36f6f8a30d32132354ec9eb17
ZaneBehen <Behenzane@gmail.com>
2022-05-15 17:42:52 +1000

    'ui'

---
+++ pages/Sidebar.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+
+export default function Sidebar() {
+    const router = useRouter()
+  return (
+   <div className="overflow-y-auto sm:py-4 sm:px-3 mr-0 bg-gray-50 dark:bg-gray-800 sm:w-64 flex invisible sm:visible">
+      <ul className="sm:space-y-2">
+    <h1 className='text-white text-xl text-center'>Manga Mix</h1>
+         <li>
+            <a href="#" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700" onClick={() => router.replace('/')}>
+               <svg className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
+               <span className="ml-3">Home</span>
+            </a>
+         </li>
+         <li>
+            <a href="#" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700" onClick={() => router.replace('/1')}>
+               <svg className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+               <span className="ml-3">View Todays Pick</span>
+            </a>
+         </li>
+         <li>
+            <a href="#" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700" onClick={() => router.replace('https://docs.api.jikan.moe/')}>
+               <svg className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd"></path></svg>
+               <span className="ml-3">API Source</span>
+            </a>
+         </li>
+         <li>
+            <a href="#" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700" onClick={() => router.replace('https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwj8kpyRxbP3AhUcILcAHe2FA18QFnoECBIQAQ&url=https%3A%2F%2Fmyanimelist.net%2F&usg=AOvVaw3XAVIyLEcyzdqn-WsmI6lP')}>
+               <svg className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z"></path></svg>
+               <span className="flex-1 ml-3 whitespace-nowrap">Rank Manga at MAL</span>
+            </a>
+         </li>
+         <li>
+            <a href="#" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700" onClick={() => router.replace('/' + Math.floor(Math.random() * 50) + 1)}>
+               <svg className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.083 9h1.946c.089-1.546.383-2.97.837-4.118A6.004 6.004 0 004.083 9zM10 2a8 8 0 100 16 8 8 0 000-16zm0 2c-.076 0-.232.032-.465.262-.238.234-.497.623-.737 1.182-.389.907-.673 2.142-.766 3.556h3.936c-.093-1.414-.377-2.649-.766-3.556-.24-.56-.5-.948-.737-1.182C10.232 4.032 10.076 4 10 4zm3.971 5c-.089-1.546-.383-2.97-.837-4.118A6.004 6.004 0 0115.917 9h-1.946zm-2.003 2H8.032c.093 1.414.377 2.649.766 3.556.24.56.5.948.737 1.182.233.23.389.262.465.262.076 0 .232-.032.465-.262.238-.234.498-.623.737-1.182.389-.907.673-2.142.766-3.556zm1.166 4.118c.454-1.147.748-2.572.837-4.118h1.946a6.004 6.004 0 01-2.783 4.118zm-6.268 0C6.412 13.97 6.118 12.546 6.03 11H4.083a6.004 6.004 0 002.783 4.118z" clipRule="evenodd"></path></svg>
+               <span className="flex-1 ml-3 whitespace-nowrap">Random Manga</span>
+            </a>
+         </li>
+         <li>
+            <a href="#" className="flex items-center hover:cursor-not-allowed p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+               <svg className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z"></path></svg>
+               <span className="flex-1 ml-3 whitespace-nowrap">Sign Up</span>
+            </a>
+         </li>
+         <li>
+            <a href="#" className="flex items-center hover:cursor-not-allowed p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+               <svg className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd"></path></svg>
+               <span className="flex-1 ml-3 whitespace-nowrap">Login</span>
+            </a>
+         </li>
+      </ul>
+   </div>
+  )
+}
---
+++ pages/[mal_id]/index.js
@@ -0,0 +1,75 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import Sidebar from '../Sidebar';
+import { useRouter } from 'next/router'
+
+const defaultEndpoint = `https://api.jikan.moe/v4/manga/`;
+
+export async function getServerSideProps( {query} ) {
+  const {mal_id} = query
+  const reccomend = mal_id + '/recommendations'
+  const res = await fetch(`${defaultEndpoint}` + mal_id);
+  const data = await res.json(); 
+  const datas = await (await fetch('https://api.jikan.moe/v4/manga/' + reccomend)).json();
+  return {
+    props: {
+      data,
+      datas
+    }
+  }
+}
+
+export default function Character({ data, datas }) {
+  const dataz = Object.values(datas)
+  const datax = dataz[0]
+  const recdata = datax.slice(0,4)
+
+  return (
+    <div className="gradient bg-gradient-to-r from-cyan-500 to-emerald-500 sm:overflow-hidden overflow-visible">
+      <Head>
+        <title></title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="gradient bg-gradient-to-r from-cyan-500 to-emerald-500 h-screen w-screen flex flex-row flex-shrink-0 sm:flex-shrink">
+      <Sidebar/>
+
+      <main className='m-0 p-0 w-full'>
+      <div className='gradient bg-gradient-to-r from-cyan-500 to-emerald-500 w-screen sm:w-full'>
+      {Object.values(data).map(result => {
+            const { title, images, mal_id, chapters, status, url, scored, synopsis } = result;
+            return (
+              <div className='sm:flex flex-col justify-center'>
+                <a href={ url } key={ url }  target="_blank" className='flex justify-center items-center cursor-default'>
+                    <img src={images.jpg.image_url} alt={`${title} Thumbnail`} key={synopsis} className='p-3 cursor-pointer transistion ml-16 duration-150 transform hover:scale-105 mr-24 md:ml-14 md:mr-36 h-[300px] w-[225px] rounded' />
+                  </a>
+                  <div className='flex flex-col mr-24 md:mr-20 ml-16 sm:ml-0'>
+                  <h1 className='text-xl break-words text-white text-center' key={chapters}>{ title }</h1>
+                  <h1 className='text-xl break-words text-white text-center' key={status}>Chapters: { chapters }</h1>
+                  <h1 className='text-xl break-words text-white text-center' key={scored}>Status : { status }</h1>
+                  <h1 className='text-xl break-words text-white text-center' key={title}>Rating : { scored } Stars</h1>
+                  </div>
+        </div>
+            )
+          })}
+          <h1 className='text-white pl-10 text-lg'>Reccomended Manga</h1>
+          <div className='flex sm:flex-row flex-col'>
+          {(Object.values(recdata)).map(resul => {
+            const { entry, url } = resul;
+            return (
+              <div>
+              <div className='flex sm:flex-none justify-center'>
+                  <Link href="/[id]" as={`/${entry.mal_id}`} className='flex justify-center items-center cursor-default'>
+                    <img src={entry.images.jpg.image_url} key={url} alt={`${entry.title} Thumbnail`} className='p-3 text-center cursor-pointer transistion duration-150 transform hover:scale-105 md:ml-14 md:mr-14 h-[250px] w-[200px] rounded' />
+                  </Link>
+                  </div>
+                  <h3 className='break-words text-white text-center'>{entry.title}</h3>
+                  </div>
+            )
+          })}
+          </div>
+          </div>
+      </main>
+      </div>
+    </div>
+  )
+}
